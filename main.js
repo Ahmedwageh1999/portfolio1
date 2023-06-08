774532fcb52b54e88fb3037de6775ccce646cb28
AhmedWagehSarhan <122888140+Ahmedwageh1999@users.noreply.github.com>
2023-06-09 00:44:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed ('.text' , {
+    strings: ['content creator' , ' kind human' , 'under progress' , 'Iam peacefull'] , 
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop: true
+})
